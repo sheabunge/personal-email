a6b8f4da129d2c6f55f9c4b515bc2611b78ee40d
Shea Bunge <info@bungeshea.com>
2013-07-01 07:15:35 +1000

    Added PHPDoc blocks

--- personal-email.php
+++ personal-email.php
@@ -11,6 +11,25 @@
  * License URI: http://opensource.org/licenses/mit-license.php
  */
 
+/**
+ * Changes the default address that WordPress uses to send emails to the one set in Settings > General.
+ *
+ * @package Personal_Email
+ * @version 1.1
+ * @author  Shea Bunge <http://bungeshea.com>
+ * @license http://opensource.org/licenses/mit-license.php MIT
+ */
+
+/**
+ * Change the default from address from
+ * wordpress@domain to the admin email address.
+ *
+ * @param  string $from_email The current from address
+ * @return string             The new from address
+ *
+ * @since  1.1                Only alter the address if the default one is being used
+ * @since  1.0
+ */
 function personal_email_from( $from_email ) {
 
 	/* Calculate the default address */
@@ -30,6 +49,15 @@ function personal_email_from( $from_email ) {
 
 add_filter( 'wp_mail_from', 'personal_email_from' );
 
+/**
+ * Alter the default name WordPress sends emails from.
+ *
+ * @param  string $from_name The current from name used to send emails
+ * @return string            The new from name used to send emails
+ *
+ * @since  1.1                Only alter the name if the default one is being used
+ * @since  1.0
+ */
 function personal_email_from_name( $from_name ) {
 	if ( 'WordPress' === $from_name ) {
 		$from_name = get_bloginfo( 'name' );
