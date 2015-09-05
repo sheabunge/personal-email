afdf992393a0646522ceedd8b81af9ae974e337d
Shea Bunge <info@bungeshea.com>
2015-09-05 20:40:00 +1000

    Update PhpDoc

--- personal-email.php
+++ personal-email.php
@@ -24,11 +24,12 @@
  * Change the default from address from
  * wordpress@domain to the admin email address.
  *
- * @param  string $from_email The current from address
- * @return string             The new from address
+ * @since 1.1 Only alter the address if the default one is being used
+ * @since 1.0
  *
- * @since  1.1                Only alter the address if the default one is being used
- * @since  1.0
+ * @param string $from_email The current from address
+ *
+ * @return string The new from address
  */
 function personal_email_from( $from_email ) {
 
@@ -52,11 +53,12 @@ add_filter( 'wp_mail_from', 'personal_email_from' );
 /**
  * Alter the default name WordPress sends emails from.
  *
- * @param  string $from_name The current from name used to send emails
- * @return string            The new from name used to send emails
+ * @since 1.1 Only alter the name if the default one is being used
+ * @since 1.0
  *
- * @since  1.1                Only alter the name if the default one is being used
- * @since  1.0
+ * @param string $from_name The current from name used to send emails
+ *
+ * @return string The new from name used to send emails
  */
 function personal_email_from_name( $from_name ) {
 	if ( 'WordPress' === $from_name ) {
